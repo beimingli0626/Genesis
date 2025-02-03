829e60311c0c628afc4ff609b6b7e6a9bd69853d
Beiming Li <edwinli0626@gmail.com>
2025-02-03 18:29:08 -0500

    2v1 with PPO in freespace doesn't learn at all, move 1v1 PPO to 1v1 skrl

--- pursuit/skrl_1v1/pursuit_env.py
+++ pursuit/skrl_1v1/pursuit_env.py
@@ -1,8 +1,16 @@
 import math
 import torch
 import genesis as gs
-import numpy as np
-import gymnasium as gym
+
+AGENT_COLORS = [
+    (1.0, 0.0, 0.0),  # red
+    (1.0, 0.5, 0.0),  # orange
+    (1.0, 1.0, 0.0),  # yellow
+    (0.0, 1.0, 0.0),  # green
+    (0.0, 0.0, 1.0),  # blue
+    (0.5, 0.0, 1.0),  # purple
+    (0.0, 0.0, 0.0),  # black
+]
 
 
 class PursuitEnv:
@@ -19,7 +27,10 @@ class PursuitEnv:
         self.num_envs = self.cfg.get("num_envs", 2)
         self.num_agents = self.cfg.get("agent", {}).get("num_agents", 1)
         self.num_observations = self.cfg.get("agent", {}).get("num_observations", 3)
-        self.num_actions = self.cfg.get("agent", {}).get("num_actions", 3)
+        self.observation_mode = self.cfg.get("agent", {}).get("observation_mode", ["rel_pos"])
+        self.update_num_observations()
+        self.num_actions = self.cfg.get("agent", {}).get("num_actions", 3) * self.num_agents
+        self.num_states = 3 * self.num_agents  # TODO: change to actual state space
 
         # create scene
         self.scene = gs.Scene(
@@ -43,14 +54,24 @@ class PursuitEnv:
         # add plane
         self.scene.add_entity(gs.morphs.Plane())
 
-        # add drone
-        self.agent = self.scene.add_entity(
-            morph=gs.morphs.Drone(
-                file="urdf/drones/cf2x.urdf",
-                fixed=True,
-                collision=True,
-            ),
-        )
+        # add agents (drones)
+        self.agents = []
+        self.possible_agents = []
+        for i in range(self.num_agents):
+            agent = self.scene.add_entity(
+                morph=gs.morphs.Drone(
+                    file="urdf/drones/cf2x.urdf",
+                    fixed=True,
+                    collision=True,
+                ),
+                surface=gs.surfaces.Rough(
+                    diffuse_texture=gs.textures.ColorTexture(
+                        color=AGENT_COLORS[i % len(AGENT_COLORS)],
+                    ),
+                ),
+            )
+            self.agents.append(agent)
+            self.possible_agents.append(f"agent_{i}")
 
         # add target / evader (rigid body)
         self.target = self.scene.add_entity(
@@ -68,8 +89,10 @@ class PursuitEnv:
         )
 
         # visualize arena
-        self.arena_size = self.cfg.get("arena", {}).get("arena_size", 2)
-        self.visualize_arena()
+        self.use_arena = self.cfg.get("arena", {}).get("use_arena", False)
+        if self.use_arena:
+            self.arena_size = self.cfg.get("arena", {}).get("arena_size", 2)
+            self.visualize_arena()
 
         # add camera
         if self.cfg.get("visualize_camera", False):
@@ -85,6 +108,7 @@ class PursuitEnv:
         self.scene.build(n_envs=self.num_envs)
 
         # prepare reward functions and multiply reward scales by dt
+        self.collision_threshold = self.cfg.get("agent", {}).get("collision_threshold", 0.2)
         self.reward_scales = self.cfg.get("reward", {}).get("reward_scales", {})
         self.reward_functions, self.episode_sums = dict(), dict()
         for name in self.reward_scales.keys():
@@ -98,38 +122,34 @@ class PursuitEnv:
         self.clip_agent_actions = self.cfg.get("agent", {}).get("clip_agent_actions", 1.5)
         self.clip_target_actions = self.cfg.get("target", {}).get("clip_target_actions", 3.0)
 
-        # initialize buffers
+        # initialize buffers, note that all below buffers should be rewrite as dict for multi-agent env, with key as agent id
         self.observation = torch.zeros((self.num_envs, self.num_observations), device=self.device, dtype=gs.tc_float)
         self.rewards = torch.zeros((self.num_envs,), device=self.device, dtype=gs.tc_float)
-        self.terminated = torch.ones((self.num_envs,), device=self.device, dtype=gs.tc_int)
+        self.terminated = torch.zeros((self.num_envs,), device=self.device, dtype=gs.tc_int)
         self.truncated = torch.zeros((self.num_envs,), device=self.device, dtype=gs.tc_int)
-
-        self.episode_length = torch.zeros((self.num_envs,), device=self.device, dtype=gs.tc_int)
-
         self.agent_actions = torch.zeros((self.num_envs, self.num_actions), device=self.device, dtype=gs.tc_float)
-        self.last_agent_actions = torch.zeros_like(self.agent_actions)  # used for smooth reward
 
         # initialize buffers need to be reset in reset()
-        self.agent_pos = torch.zeros((self.num_envs, 3), device=self.device, dtype=gs.tc_float)
+
+        self.agent_pos = torch.zeros((self.num_envs, self.num_agents, 3), device=self.device, dtype=gs.tc_float)
         self.target_pos = torch.zeros((self.num_envs, 3), device=self.device, dtype=gs.tc_float)
-        self.rel_pos = torch.zeros((self.num_envs, 3), device=self.device, dtype=gs.tc_float)
+        self.rel_pos = torch.zeros((self.num_envs, self.num_agents, 3), device=self.device, dtype=gs.tc_float)
+        self.min_dist = torch.zeros((self.num_envs,), device=self.device, dtype=gs.tc_float)
+        self.avg_dist = torch.zeros((self.num_envs,), device=self.device, dtype=gs.tc_float)
         self.last_agent_pos = torch.zeros_like(self.agent_pos)
         self.last_target_pos = torch.zeros_like(self.target_pos)
         self.last_rel_pos = torch.zeros_like(self.rel_pos)
+        self.last_min_dist = torch.zeros_like(self.min_dist)
+
+        self.episode_length = torch.zeros((self.num_envs,), device=self.device, dtype=gs.tc_int)
+        self.last_agent_actions = torch.zeros_like(self.agent_actions)  # used for smooth reward
         self.extras = dict()  # extra information for logging
 
-    @property
-    def observation_space(self):
-        return self.num_observations
-
-    @property
-    def action_space(self):
-        return self.num_actions
-
-    @property
-    def state_space(self):
-        # TODO: change to actual state space
-        return self.agent_pos.shape[-1]
+    def update_num_observations(self):
+        if "rel_pos" in self.observation_mode:
+            self.num_observations = self.num_agents * 3
+        if "rel_pos_agents" in self.observation_mode:
+            self.num_observations += self.num_agents * self.num_agents * 3
 
     def step(self, actions):
         """Step environment
@@ -145,11 +165,15 @@ class PursuitEnv:
             extras: Dictionary containing extra information for logging
         """
 
-        # apply agent action
+        # apply agent actions
         self.agent_actions = torch.clip(actions, -self.clip_agent_actions, self.clip_agent_actions)
-        self.agent_actions[:, 2] = 0.0  # keep Z constant
-        new_agent_pos = self.agent_pos + self.dt * self.agent_actions
-        self.agent.set_pos(new_agent_pos, zero_velocity=True)
+        actions_reshaped = self.agent_actions.view(self.num_envs, self.num_agents, 3)
+        actions_reshaped[..., 2] = 0.0  # keep Z constant for all agents
+
+        # Update positions for all agents
+        new_agent_pos = self.agent_pos + self.dt * actions_reshaped
+        for i in range(self.num_agents):
+            self.agents[i].set_pos(new_agent_pos[:, i], zero_velocity=True)
 
         # calculate and apply target action
         target_actions = self.get_repulsive_action()
@@ -165,9 +189,22 @@ class PursuitEnv:
         self.last_agent_pos[:] = self.agent_pos[:]
         self.last_target_pos[:] = self.target_pos[:]
         self.last_rel_pos[:] = self.rel_pos[:]
-        self.agent_pos[:] = self.agent.get_pos()
-        self.target_pos[:] = self.target.get_pos()
-        self.rel_pos = self.target_pos - self.agent_pos
+        self.last_min_dist[:] = self.min_dist[:]
+
+        # query agent positions
+        for i in range(self.num_agents):
+            self.agent_pos[:, i] = self.agents[i].get_pos()
+        self.target_pos[:] = self.target.get_pos()  # [num_envs, 3]
+        self.rel_pos = self.target_pos.unsqueeze(1) - self.agent_pos  # [num_envs, num_agents, 3]
+
+        # calculate relative positions between all agents
+        # [num_envs, num_agents, num_agents, 3] where rel_pos_agents[i,j,k] is pos of agent k relative to agent j in env i
+        self.rel_pos_agents = self.agent_pos.unsqueeze(2) - self.agent_pos.unsqueeze(1)
+
+        # compute min distance of any agent to target
+        rel_dist = torch.norm(self.rel_pos[..., :2], p=2, dim=-1)  # dist in xy plane, [num_envs, num_agents]
+        self.min_dist = torch.amin(rel_dist, dim=1)  # [num_envs]
+        self.avg_dist = rel_dist.mean(dim=1)  # [num_envs]
 
         # compute observations
         self.compute_observations()
@@ -180,10 +217,7 @@ class PursuitEnv:
             self.episode_sums[name] += rew
 
         # check truncate and reset
-        self.terminated = (
-            torch.norm(self.rel_pos[:, :2], dim=1)
-            < self.at_target_threshold  # distance in xy within threshold = capture
-        )
+        self.terminated = self.min_dist < self.at_target_threshold
         self.truncated = self.episode_length > self.max_episode_length
 
         # place before reset_idx because it overwrites last_agent_actions for finished environments for correct reward computation at the next step
@@ -207,7 +241,10 @@ class PursuitEnv:
 
         Note that observation should be deepcopy, otherwise it will point to the same memory address as rel_pos
         """
-        self.observation[:] = self.rel_pos[:]
+        if "rel_pos" in self.observation_mode:
+            self.observation[:, : 3 * self.num_agents] = self.rel_pos.view(self.num_envs, -1)
+        if "rel_pos_agents" in self.observation_mode:
+            self.observation[:, 3 * self.num_agents :] = self.rel_pos_agents.view(self.num_envs, -1)
 
     def reset_idx(self, envs_idx):
         """Reset environments
@@ -220,24 +257,41 @@ class PursuitEnv:
         if len(envs_idx) == 0:
             return
 
-        # sample initial positions for agent and target
-        range = 1.5
-        self.agent_init_pos = torch.ones((len(envs_idx), 3), device=self.device)
-        self.agent_init_pos[:, :2] = torch.empty((len(envs_idx), 2), device=self.device).uniform_(-range, range)
-        self.target_init_pos = torch.ones((len(envs_idx), 3), device=self.device)
-        self.target_init_pos[:, :2] = torch.empty((len(envs_idx), 2), device=self.device).uniform_(-range, range)
+        # sample initial positions for agents and target
+        if self.use_arena:
+            pos_range = 1.0
+            self.target_init_pos = torch.empty((len(envs_idx), 3), device=self.device).uniform_(-pos_range, pos_range)
+            self.agent_init_pos = torch.empty((len(envs_idx), self.num_agents, 3), device=self.device).uniform_(
+                -pos_range, pos_range
+            )
+        else:
+            # Initialize target at origin
+            self.target_init_pos = torch.zeros((len(envs_idx), 3), device=self.device)
 
-        # reset agent
+            # Initialize agents 1-2 meters away from target in xy plane
+            angles = torch.rand((len(envs_idx), self.num_agents), device=self.device) * 2 * math.pi
+            distances = torch.rand((len(envs_idx), self.num_agents), device=self.device) + 1.0  # 1-2 meters
+            self.agent_init_pos = torch.zeros((len(envs_idx), self.num_agents, 3), device=self.device)
+            self.agent_init_pos[..., 0] = distances * torch.cos(angles)  # x coordinate
+            self.agent_init_pos[..., 1] = distances * torch.sin(angles)  # y coordinate
+        self.target_init_pos[..., 2] = 1.0  # Set z-coordinate to 1.0
+        self.agent_init_pos[..., 2] = 1.0  # Set z-coordinate to 1.0
+
+        # reset agents
         self.agent_pos[envs_idx] = self.agent_init_pos
-        self.agent.set_pos(self.agent_pos[envs_idx], zero_velocity=True, envs_idx=envs_idx)
-        self.agent.zero_all_dofs_velocity(envs_idx)
+        for i in range(self.num_agents):
+            self.agents[i].set_pos(self.agent_pos[envs_idx, i], zero_velocity=True, envs_idx=envs_idx)
+            self.agents[i].zero_all_dofs_velocity(envs_idx)
 
         # reset target
         self.target_pos[envs_idx] = self.target_init_pos
         self.target.set_pos(self.target_pos[envs_idx], zero_velocity=True, envs_idx=envs_idx)
         self.target.zero_all_dofs_velocity(envs_idx)
 
-        self.rel_pos[envs_idx] = self.target_pos[envs_idx] - self.agent_pos[envs_idx]
+        self.rel_pos[envs_idx] = self.target_pos[envs_idx].unsqueeze(1) - self.agent_pos[envs_idx]
+        rel_dist = torch.norm(self.rel_pos[envs_idx, :, :2], p=2, dim=-1)  # [num_envs_idx, num_agents]
+        self.min_dist[envs_idx] = torch.amin(rel_dist, dim=-1)  # [num_envs_idx]
+        self.avg_dist[envs_idx] = rel_dist.mean(dim=-1)  # [num_envs_idx]
 
         self.last_agent_actions[envs_idx] = 0  # initial last action should be zero for a new episode
         self.episode_length[envs_idx] = 0
@@ -251,25 +305,27 @@ class PursuitEnv:
     def get_repulsive_action(self):
         """Compute repulsive action for the evader
 
-        The closer pursuer is to the evader, the stronger the force, refer to https://arxiv.org/pdf/2010.08193
+        The closer pursuer is to the evader, the stronger the force.
+        With multiple pursuers, sum up the repulsive forces from each pursuer.
 
         Returns:
-            force: repulsive action
+            force: repulsive force from all pursuers plus arena boundary force
         """
-        # pursuer force
-        force_pursuer = self.target_pos - self.agent_pos
-        force_pursuer[:, 2] = 0
-        norm = torch.norm(force_pursuer, dim=1, keepdim=True) ** 2 + 1e-5
-        force_pursuer = force_pursuer / norm
+        # Calculate pursuer forces for all agents at once
+        force_pursuer = torch.zeros((self.num_envs, 3), device=self.device, dtype=gs.tc_float)
+        rel_pos_xy = self.rel_pos[..., :2]  # [num_envs, num_agents, 2]
+        norm = torch.norm(rel_pos_xy, p=2, dim=-1, keepdim=True) ** 2 + 1e-5  # [num_envs, num_agents, 1]
+        force_pursuer[..., :2] = (rel_pos_xy / norm).sum(dim=1)  # sum over agents, result is [num_envs, 2]
 
         # arena force, receive very large force when out of arena
         force_arena = torch.zeros_like(force_pursuer)
-        target_origin_dist = torch.norm(self.target_pos[..., :2], dim=-1, keepdim=True)
-        force_arena_vec = -self.target_pos[..., :2] / (target_origin_dist + 1e-5)  # unit vector pointing to center
-        out_of_arena = torch.norm(self.target_pos[..., :2], dim=-1, keepdim=True) > self.arena_size
-        force_arena[..., :2] = out_of_arena.float() * force_arena_vec * (1 / 1e-5) + (
-            ~out_of_arena
-        ).float() * force_arena_vec * (1 / ((self.arena_size - target_origin_dist) + 1e-5))
+        if self.use_arena:
+            target_origin_dist = torch.norm(self.target_pos[..., :2], p=2, dim=-1, keepdim=True)
+            force_arena_vec = -self.target_pos[..., :2] / (target_origin_dist + 1e-5)  # unit vector pointing to center
+            out_of_arena = torch.norm(self.target_pos[..., :2], p=2, dim=-1, keepdim=True) > self.arena_size
+            force_arena[..., :2] = out_of_arena.float() * force_arena_vec * (1 / 1e-5) + (
+                ~out_of_arena
+            ).float() * force_arena_vec * (1 / ((self.arena_size - target_origin_dist) + 1e-5))
 
         force = force_pursuer + force_arena
 
@@ -280,6 +336,14 @@ class PursuitEnv:
         return force
 
     # ------------ reward functions ----------------
+    def _reward_distance(self):
+        """Reward (penalty) for distance to target
+
+        Returns:
+            reward: Negative average distance to target for all agents
+        """
+        return self.avg_dist
+
     def _reward_target(self):
         """Reward for moving towards the target
 
@@ -287,7 +351,7 @@ class PursuitEnv:
         NOTE: this is not a good reward function, it is just for testing; it will drive the agent to
         stay within certain distance from the target (why?)
         """
-        target_rew = torch.sum(torch.square(self.last_rel_pos), dim=1) - torch.sum(torch.square(self.rel_pos), dim=1)
+        target_rew = self.last_min_dist - self.min_dist
         return target_rew
 
     def _reward_smooth(self):
@@ -296,7 +360,7 @@ class PursuitEnv:
         Reward is the square of difference in agent action between current and previous step
         """
         smooth_rew = torch.sum(torch.square(self.agent_actions - self.last_agent_actions), dim=1)
-        return smooth_rew
+        return -smooth_rew
 
     def _reward_capture(self):
         """Reward for capturing the target
@@ -304,9 +368,22 @@ class PursuitEnv:
         Returns:
             reward: 1 if target is captured, 0 otherwise
         """
-        return (
-            torch.norm(self.rel_pos[:, :2], dim=1) < self.at_target_threshold
-        )  # distance in xy within threshold = capture
+        return self.min_dist < self.at_target_threshold  # distance in xy within threshold = capture
+
+    def _reward_collision(self):
+        """Reward (penalty) for agents getting too close to each other
+
+        Returns:
+            reward: -1 for each pair of agents that are too close
+        """
+        distances = torch.norm(self.rel_pos_agents[..., :2], p=2, dim=-1)  # [num_envs, num_agents, num_agents]
+        close_pairs = (distances < self.collision_threshold).sum(dim=(1, 2))  # [num_envs]
+
+        # Subtract self-distances and divide by 2 (since matrix is symmetric)
+        num_collisions = (close_pairs - self.num_agents) / 2
+
+        # Return negative reward for each collision
+        return num_collisions
 
     # ------------ debug visualization ----------------
     def visualize_forces(self, force_pursuer, force_arena, total_force, env_idx=0):
@@ -364,3 +441,28 @@ class PursuitEnv:
 
     def close(self):
         pass
+
+    # # TODO: define observations spaces, action spaces, shared observation spaces
+    # # ------------ multi-agent env required properties ------------
+    # @property
+    # def observation_spaces(self):
+    #     return self.num_observations
+
+    # @property
+    # def action_spaces(self):
+    #     return self.num_actions
+
+    # ------------ single-agent env required properties ------------
+    @property
+    def state_space(self):
+        # TODO: change to actual state space
+        # for multiagent env, this is the global state space
+        return self.agent_pos.shape[-1]
+
+    @property
+    def observation_space(self):
+        return self.num_observations
+
+    @property
+    def action_space(self):
+        return self.num_actions
--- pursuit/skrl_1v1/pursuit_train.py
+++ pursuit/skrl_1v1/pursuit_train.py
@@ -42,9 +42,11 @@ class Policy(GaussianMixin, Model):
         GaussianMixin.__init__(self, clip_actions, clip_log_std, min_log_std, max_log_std, reduction)
 
         self.net = nn.Sequential(
-            nn.Linear(self.num_observations, 128),
+            nn.Linear(self.num_observations, 512),
             nn.ELU(),
-            nn.Linear(128, 128),
+            nn.Linear(512, 256),
+            nn.ELU(),
+            nn.Linear(256, 128),
             nn.ELU(),
             nn.Linear(128, self.num_actions),
         )
@@ -60,7 +62,13 @@ class Value(DeterministicMixin, Model):
         DeterministicMixin.__init__(self, clip_actions)
 
         self.net = nn.Sequential(
-            nn.Linear(self.num_observations, 128), nn.ELU(), nn.Linear(128, 128), nn.ELU(), nn.Linear(128, 1)
+            nn.Linear(self.num_observations, 512),
+            nn.ELU(),
+            nn.Linear(512, 256),
+            nn.ELU(),
+            nn.Linear(256, 128),
+            nn.ELU(),
+            nn.Linear(128, 1),
         )
 
     def compute(self, inputs, role):
@@ -69,14 +77,14 @@ class Value(DeterministicMixin, Model):
 
 def get_train_cfg(log_dir, experiment_name):
     train_cfg_dict = {
-        "rollouts": 100,  # number of rollouts before updating
+        "rollouts": 10,  # number of rollouts before updating
         "learning_epochs": 5,  # number of learning epochs during each update
         "mini_batches": 4,  # number of mini batches during each learning epoch
         "discount_factor": 0.99,  # discount factor (gamma)
         "lambda": 0.95,  # TD(lambda) coefficient (lam) for computing returns and advantages
         "learning_rate": 3e-4,  # learning rate
         "learning_rate_scheduler": KLAdaptiveRL,  # learning rate scheduler class
-        "learning_rate_scheduler_kwargs": {"kl_threshold": 0.008},  # learning rate scheduler's kwargs
+        "learning_rate_scheduler_kwargs": {"kl_threshold": 0.01, "min_lr": 1e-5},  # learning rate scheduler's kwargs
         "state_preprocessor": None,  # state preprocessor class (see skrl.resources.preprocessors)
         "state_preprocessor_kwargs": {},  # state preprocessor's kwargs (e.g. {"size": env.observation_space})
         "value_preprocessor": None,  # value preprocessor class (see skrl.resources.preprocessors)
@@ -86,10 +94,10 @@ def get_train_cfg(log_dir, experiment_name):
         "grad_norm_clip": 1.0,  # clipping coefficient for the norm of the gradients
         "ratio_clip": 0.2,  # clipping coefficient for computing the clipped surrogate objective
         "value_clip": 0.2,  # clipping coefficient for computing the value loss (if clip_predicted_values is True)
-        "clip_predicted_values": False,  # clip predicted values during value loss computation
-        "entropy_loss_scale": 0.0,  # entropy loss scaling factor
+        "clip_predicted_values": True,  # clip predicted values during value loss computation
+        "entropy_loss_scale": 0.01,  # entropy loss scaling factor
         "value_loss_scale": 1.0,  # value loss scaling factor
-        "kl_threshold": 0,  # KL divergence threshold for early stopping
+        "kl_threshold": 0.0,  # KL divergence threshold for early stopping, prevent overfitting
         "rewards_shaper": None,  # rewards shaping function: Callable(reward, timestep, timesteps) -> reward
         "time_limit_bootstrap": False,  # bootstrap at timeout termination (episode truncation)
         "mixed_precision": True,  # enable automatic mixed precision for higher performance
@@ -99,7 +107,7 @@ def get_train_cfg(log_dir, experiment_name):
             "write_interval": 40,  # TensorBoard writing interval (timesteps), "auto" save 100 records
             "checkpoint_interval": "auto",  # interval for checkpoints (timesteps), "auto" save 10 checkpoints
             "store_separately": False,  # whether to store checkpoints separately
-            "wandb": False,  # whether to use Weights & Biases
+            "wandb": True,  # whether to use Weights & Biases
             "wandb_kwargs": {  # wandb kwargs
                 "project": "pursuit-evasion",
                 "entity": WANDB_ENTITY,
@@ -117,23 +125,29 @@ def get_env_cfg():
         "dt": 0.01,
         # agent
         "agent": {
-            # "num_agents": 2,
-            "num_observations": 3,
-            "num_actions": 3,
-            "at_target_threshold": 0.3,
-            "clip_agent_actions": 1.5,
+            "num_agents": 1,
+            "num_observations": 3,  # number of observations per agent
+            "num_actions": 3,  # number of actions per agent
+            "at_target_threshold": 0.5,
+            "clip_agent_actions": 3.0,
+            "observation_mode": ["rel_pos"],
+            "collision_threshold": 0.2,
         },
         "target": {
-            "clip_target_actions": 2.0,
+            "clip_target_actions": 2.5,
         },
         "arena": {
+            "use_arena": False,
             "arena_size": 2.0,
         },
         "reward": {
             "reward_scales": {
-                "target": 10.0,
-                "smooth": -1e-4,
-                "capture": 1.0,
+                "distance": -1,
+                # "target": 10.0,
+                # "smooth": 1e-4,  # two training phase, only consider smooth reward in the second phase
+                "capture": 1.0,  # capture 100 is too high, make the agent overfit to move to one single direction
+                # capture 10 is too high for two agents with arena
+                # "collision": -10,
             },
         },
         # visualization
@@ -198,7 +212,7 @@ def main():
     )
 
     # create trainer, note that genesis is agnostic to "headless"
-    cfg_trainer = {"timesteps": 8000, "headless": True}
+    cfg_trainer = {"timesteps": 16000, "headless": True}
     trainer = SequentialTrainer(cfg=cfg_trainer, env=env, agents=agent)
 
     # train
